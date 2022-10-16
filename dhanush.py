3542679278e7ecae3f8df5365cd25c5af0378dd4
Rinshad <rinshadis19@gmail.com>
2022-10-16 19:45:07 +0530

    second version

--- dhanush.py
+++ dhanush.py
@@ -1,9 +1,17 @@
 
-#!welcome to dhanush's coffee shop
+#!welcome to dhanush's coffee shop....
 
 name = input("enter your name :")
 if name == 'dhanush' or name == 'Dhanush':
-    print("Hello sir ! make you way to the room your food is on the way :)")
+    print("Hello Boss ! make you way to the room your food will be on its way :)")
+    menu1 = ['Lime tea','mint lime','falooda']
+    print("Today your special items are ",menu1)
+    order1 = input("What shall i order from these special items : ")
+    for options in menu1:
+        if order1 in menu1:
+            print(f"Your {order1} will be sent soon Boss!")
+        elif order1 not in menu1:
+            print("Sorry,Boss we will find a way to arrange your request..")
 else:
     print(f"Greeting {name} welcome to Dhanush's Coffee Shop")
     menu = ['masala tea','Cappucchino','Latte','Black coffee']
